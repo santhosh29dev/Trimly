d322d508297cd462bb2a8ae5250913b39dee2bf2
santhosh <santhosh29.dev@gmail.com>
2026-03-01 21:32:09 +0530

    feat: connection established with middleware(#03)

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import urlRoutes from "./routes/url.js";
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+  }),
+);
+
+app.use(express.json());
+
+app.use("/", urlRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("success");
+    app.listen(process.env.PORT, () => {
+      console.log("server is runnig");
+    });
+  })
+  .catch((err) => {
+    console.error("error connecting", err);
+  });
