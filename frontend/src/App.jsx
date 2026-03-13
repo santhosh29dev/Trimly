1aa78f50fff95f1e95d108300703a8dbbddfdddf
santhosh <santhosh29.dev@gmail.com>
2026-03-13 22:36:34 +0530

    fix : Frontend design and fuctions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,14 @@ const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 function App() {
   const [url, setUrl] = useState("");
-  const [shortUrl, setshortUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
   const [copied, setCopied] = useState(false);
   const [qrImage, setQrImage] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleshorten = async () => {
-    if (!url) return;
+  const handleShorten = async () => {
+    if (!url || loading) return;
+    setLoading(true);
 
     try {
       const res = await axios.post(`${API_BASE_URL}/shorten`, {
@@ -20,71 +22,73 @@ function App() {
       });
 
       const newShortUrl = res.data.shortUrl;
-      setshortUrl(newShortUrl);
+      setShortUrl(newShortUrl);
       setCopied(false);
 
       const qr = await QRCodeGenerator.toDataURL(newShortUrl);
       setQrImage(qr);
     } catch (err) {
       console.log(err);
-      alert("Something Went Wrong");
+      alert("Something went wrong");
+    } finally {
+      setLoading(false);
     }
   };
 
   const handleCopy = () => {
     navigator.clipboard.writeText(shortUrl);
     setCopied(true);
-    setTimeout(() => {
-      setCopied(false);
-    }, 2000);
+    setTimeout(() => setCopied(false), 2000);
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-6">
-      <h1 className="text-4xl font-bold mb-4 text-center">URL Shortner</h1>
-
+    <div className="min-h-screen flex flex-col items-center justify-center p-6 gap-6">
+      <h1 className="text-4xl font-bold mb-4 text-center">URL SHORTENER</h1>
       <div className="flex flex-col gap-3 w-full max-w-3xl">
         <input
           type="text"
           className="input input-success w-full"
-          placeholder="Enter The Long URL"
+          placeholder="Enter long URL"
           value={url}
           onChange={(e) => setUrl(e.target.value)}
         />
         <button
+          onClick={handleShorten}
           className="btn btn-primary w-full sm:auto"
-          onClick={handleshorten}
+          disabled={loading}
         >
           Shorten
         </button>
       </div>
-
       {shortUrl && (
         <div className="flex flex-col items-center max-w-3xl w-full">
-          <p font-medium mb-2>
-            Your Short Link
-          </p>
-          <a className="link link-primary break-all" href={shortUrl}>
+          <p className="font-medium mb-2">Your short link:</p>
+          <a
+            className="link link-primary break-all"
+            target="_blank"
+            href={shortUrl}
+          >
             {shortUrl}
           </a>
           <button
             onClick={handleCopy}
-            className={`btn mt-2 w-full ${copied ? "btn-success" : "btn-secondary"}`}
+            className={`btn mt-2 w-full ${
+              copied ? "btn-success" : "btn-secondary"
+            }`}
           >
-            {copied ? "Copied" : "Copy"}
+            {copied ? "Copied!" : "Copy"}
           </button>
 
           <div className="bg-white p-4 rounded-lg shadow mt-6">
             <p className="mb-2 text-center font-semibold text-gray-800">
-              Scan QR Code
+              Scan QR Code:
             </p>
             <QRCode value={shortUrl} size={180} />
           </div>
-
           {qrImage && (
             <a
               className="btn btn-accent mt-3 w-full"
-              download="qr.code.png"
+              download="qr-code.png"
               href={qrImage}
             >
               Download QR Code
